6cf9f8a9b6c44eebee093cfcba445f912e9d5bf7
Song Shao Ting <qwe820610@gmail.com>
2020-12-24 13:27:34 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const post = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname:'.hbs'}))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.send('This is my web')
+})
+
+app.listen(post, () => {
+    console.log(`Express is running on http://localhost:${post}`)
+})
