50b0ecf8218c1c8020756bab6aff43e4d53ff2b5
Song Shao Ting <qwe820610@gmail.com>
2020-12-24 13:36:41 +0800

    feat: add mongoose

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/account_login', { useNewUrlParser: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error')
+})
+
+db.once('open', () => {
+    console.log('mongodb conneted')
+})
+
+module.exports = db
